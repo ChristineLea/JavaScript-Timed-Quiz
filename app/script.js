3ca73a564a63a6d3af993dae90ab2012121076e7
Christine Lea <christine87@live.com.au>
2023-06-25 12:43:10 +1000

    "add constant variables"

---
+++ app/script.js
@@ -0,0 +1,18 @@
+// TOGGLE DISPLAY ON/OFF CONSTANTS
+const sectionOne = document.querySelector(".one");
+const sectionTwo = document.querySelector(".two");
+const sectionThree = document.querySelector(".two");
+const sectionFour = document.querySelector(".two");
+const sectionFive = document.querySelector(".two");
+
+// QUIZ QA CONSTANTS
+const questionEl = document.querySelector("#question");
+const option1 = document.querySelector(".option1");
+const option2 = document.querySelector(".option2");
+const option3 = document.querySelector(".option3");
+const option4 = document.querySelector(".option4");
+const output = document.querySelector("#output");
+output.setAttribute(
+	"style",
+	"font-size: 24px; font-style: italic; color: #66023c; font-weight: bolder;"
+);
